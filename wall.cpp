be33aa6f72f5d75b079d877e7ede5171600a26f0
nekofood <sonicroblox@gmail.com>
2024-02-08 20:11:25 +0800

    create constructor

--- wall.cpp
+++ wall.cpp
@@ -1,3 +1,11 @@
 #include "wall.h"
+#include <cmath>
 
-Wall::Wall() {}
+Wall::Wall(qreal x1pos, qreal y1pos, qreal x2pos, qreal y2pos) {
+    x1 = x1pos;
+    y1 = y1pos;
+    x2 = x2pos;
+    y2 = y2pos;
+    angle = atan2(y2-y1, x2-x1); //Calculate angle in radians
+
+}
--- wall.h
+++ wall.h
@@ -6,7 +6,13 @@
 class Wall : public QGraphicsItem
 {
 public:
-    Wall();
+    Wall(qreal x1, qreal y1, qreal x2, qreal y2);
+private:
+    qreal x1;
+    qreal x2;
+    qreal y1;
+    qreal y2;
+    qreal angle;
 };
 
 #endif // WALL_H
